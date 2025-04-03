2dd17094bda0c4f07eec599f77a994e3c9ccdfd5
desivar <jillian_desire@yahoo.es>
2025-04-03 09:32:32 -0600

    add productRoutes.js, file

---
+++ pet-shop-admin-backend/server.js
@@ -0,0 +1,10 @@
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Shop Backend API', // if you wish you could cahnge the title here
+        version: '1.0.0',
+      },
+    },
+    apis: ['./*.js'],
+  };
